14759a62c09887728e7b8505c4af0c8d71dd84bf
WilsonXicara <wilsonx192@gmail.com>
2019-11-25 13:05:29 -0600

    Maquinarias alquiladas y disponibles

--- mrental/machineries/views/machineries.py
+++ mrental/machineries/views/machineries.py
@@ -3,6 +3,8 @@ Machinery views.
 """
 # Django REST Framework
 from rest_framework import mixins, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 # Models
 from mrental.machineries.models import Machinery
 # Serializers
@@ -24,3 +26,34 @@ class MachineryViewSet(mixins.CreateModelMixin,     # Crear nuevos registros
         """
         queryset = Machinery.objects.filter(is_active=True)
         return queryset
+    
+    @action(detail=False, methods=['get'])
+    def rents(self, request, *args, **kwargs):
+        """
+        Get the list of available and rented machinery.
+        """
+        values = ('code','machinery_type','name','default_amount',)
+        rented = Machinery.objects.filter(
+            is_active=True,
+            is_rented=True
+        ).values(*values)
+        not_rented = Machinery.objects.filter(
+            is_active=True,
+            is_rented=False
+        ).values(*values)
+        rented_json, not_rented_json = [], []
+        for machinery in rented:
+            machinery_data = {}
+            for value in values:
+                machinery_data[value] = machinery[value]
+            rented_json.append(machinery_data)
+        for machinery in not_rented:
+            machinery_data = {}
+            for value in values:
+                machinery_data[value] = machinery[value]
+            not_rented_json.append(machinery_data)
+        data = {
+            'rented': rented_json,
+            'not_rented': not_rented_json
+        }
+        return Response(data=data)
